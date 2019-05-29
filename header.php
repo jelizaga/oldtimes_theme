9b1cb2cff5893af8e6250c98482dc728cb83449c
Joel Elizaga <joelizaga@hotmail.com>
2019-05-29 10:56:53 -0700

    h1 on non-index title instead of p.

--- header.php
+++ header.php
@@ -24,35 +24,44 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'old-times' ); ?></a>
 
+	<!-- NAV ----------------------------------------------------------------->
+	<nav id="site-navigation" class="main-navigation">
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'old-times' ); ?></button>
+		<?php
+		wp_nav_menu( array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'primary-menu',
+		) );
+		?>
+	</nav><!-- #site-navigation -->
+
+	<!-- HEADER -------------------------------------------------------------->
 	<header id="masthead" class="site-header">
+
 		<div class="site-branding">
 			<?php
+			
 			the_custom_logo();
+
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			endif;
+
 			$old_times_description = get_bloginfo( 'description', 'display' );
+
 			if ( $old_times_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $old_times_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
+			
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'old-times' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
